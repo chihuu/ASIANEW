a924d292f5a524286743f94d157d476a3e5c7838
huuccqp <huuccqb@gmail.com>
2017-08-30 15:49:47 +0700

    fix libary

--- android/app/src/main/java/com/asia/MainApplication.java
+++ android/app/src/main/java/com/asia/MainApplication.java
@@ -3,6 +3,7 @@ package com.asia;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.corbt.keepawake.KCKeepAwakePackage;
 import com.github.yamill.orientation.OrientationPackage;
 import com.brentvatne.react.ReactVideoPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new KCKeepAwakePackage(),
             new OrientationPackage(),
             new ReactVideoPackage(),
